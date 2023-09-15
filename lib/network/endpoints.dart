7478d7288b3715b97e8e7abab7f9cf7748a86412
Diana <Diana@ascentitech.com>
2023-09-15 15:14:25 -0500

    final

--- lib/network/endpoints.dart
+++ lib/network/endpoints.dart
@@ -2,9 +2,6 @@ import "package:flutter_dotenv/flutter_dotenv.dart";
 
 /// picsum.photos api doc: https://picsum.photos/
 class EndPoints{
-  final String apiKey ="39450027-fd14e45bebf2c8249294a582a";
-
-      //dotenv.env['API_KEY'] ?? 'API KEY not found';
 
   String baseUrl = "https://picsum.photos/";
 
